4ed136448876f2368463981e93c43a78b56041b3
HKSOLDIER <hemanthchitta98@gmail.com>
2026-05-27 23:55:04 +0530

    EmailService debug added 2

--- backend/src/main/java/com/securevault/service/EmailService.java
+++ backend/src/main/java/com/securevault/service/EmailService.java
@@ -1,10 +1,10 @@
-// package com.securevault.service;
+package com.securevault.service;
 
-// import lombok.RequiredArgsConstructor;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.mail.SimpleMailMessage;
-// import org.springframework.mail.javamail.JavaMailSender;
-// import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 // @Service
 // @RequiredArgsConstructor
