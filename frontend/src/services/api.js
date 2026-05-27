200203bcabb5296bebf20a0ef6dc71e32ae455d3
HKSOLDIER <hemanthchitta98@gmail.com>
2026-05-28 00:07:23 +0530

    Added the base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+// const BASE_URL = 'http://localhost:8080';
+
+const BASE_URL = 'https://securevault-ics5.onrender.com';
 
 export const api = axios.create({
   baseURL: `${BASE_URL}/api`,
