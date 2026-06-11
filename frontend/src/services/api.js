cd0eca7a45c30c95850efe70eedc678ea9dc84d6
HKSOLDIER <hemanthchitta98@gmail.com>
2026-06-11 21:29:04 +0530

    Removed all data secrecy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // const BASE_URL = 'http://localhost:8080';
 
-const BASE_URL = 'https://securevault-ics5.onrender.com';
+// const BASE_URL = 'https://securevault-ics5.onrender.com';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const api = axios.create({
   baseURL: `${BASE_URL}/api`,
